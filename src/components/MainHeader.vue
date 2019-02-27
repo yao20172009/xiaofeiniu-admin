209c32683a02f2aa8abe69ff66e5fd7be9913856
Jilly <359407364@qq.com>
2019-02-27 20:48:01 +0800

    admin

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="xfn-main-header">
+    <h3 class="xfn-title">{{$store.state.globalSettings.appName}}后台管理系统</h3>
+    <div class="xfn-right">当前用户：{{$store.state.adminName}}
+      <el-button size="mini" @click="quit">退出</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    quit(){
+      this.$store.commit('setAdminName','');
+      this.$router.push('/login');
+    }
+  },
+ 
+}
+</script>
+
+<style scoped lang="scss">
+$header-height:60px;
+.xfn-main-header{
+  background: #DCDFE6;
+  border-radius: 2px;
+  line-height: $header-height;
+  height: $header-height;
+  padding: 0 6px;
+  .xfn-title{
+    color:#303133;
+    float: left;
+    margin: 0;
+    text-align: center
+  }
+  .xfn-right{
+    float: right;
+  }
+
+}
+</style>
--- src/views/GlobalSettings.vue
+++ src/views/GlobalSettings.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="app-notfound">
-    <h1>404 NotFound</h1>
+  <div class="settings">
+    <el-breadcrumb>
+      <el-breadcrumb-item to="/main">首页 </el-breadcrumb-item>
+      <el-breadcrumb-item >全局设置 </el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card shadow="never">
+      <el-form lable-width="100px">
+        <el-form-item label="应用名称：">
+          <el-input v-model="formDate.appName"></el-input>
+        </el-form-item>
+        <el-form-item label="数据API地址：">
+          <el-input v-model="formDate.apiUrl"></el-input>
+        </el-form-item>
+        <el-form-item label="管理后台地址：">
+          <el-input v-model="formDate.adminUrl"></el-input>
+        </el-form-item>
+        <el-form-item label="客户APP地址：">
+          <el-input v-model="formDate.appUrl"></el-input>
+        </el-form-item>
+        <el-form-item label="ICP备案号：">
+          <el-input v-model="formDate.icp"></el-input>
+        </el-form-item>
+        <el-form-item label="版权申明：">
+          <el-input v-model="formDate.copyright"></el-input>
+        </el-form-item>
+        <el-button type="primary">提交</el-button>
+        <el-button>取消</el-button>
+      </el-form>     
+    </el-card>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      formDate:{}
+    }
+  },
+  mounted() {
+    this.formDate=this.$store.state.globalSettings;
+  },
+}
+</script>
